4aace6a50242858b471a068a34e82fd6ef9f639a
Giau Tran Minh <giau.tmg@gmail.com>
2018-09-13 13:59:48 +0700

    Add release channel to url

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ module.exports = ({ url, iosManifest, config }) => {
   const { botToken, chatIds } = config;
   const fetch = require('node-fetch');
 
-  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${url}`;
+  const releaseChannel = iosManifest.releaseChannel || 'default';
+  const queryString = releaseChannel === 'default' ? '' :
+    '?release-channel='+encodeURIComponent(releaseChannel);
+
+  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${url}${queryString}`;
 
   return Promise.all((chatIds || []).map((chatId) => {
     return fetch(`https://api.telegram.org/bot${botToken}/sendPhoto`, {
@@ -13,7 +17,7 @@ module.exports = ({ url, iosManifest, config }) => {
       body: JSON.stringify({
         chat_id: chatId,
         photo: qrUrl,
-        caption: `${iosManifest.name} v${iosManifest.version} published to ${url}`,
+        caption: `${iosManifest.name} v${iosManifest.version} published to ${url}${queryString}`,
       }),
     }).then((response) => response.json()).then((data) => {
       if (data.ok) {
