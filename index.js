715b4e99e05629b5d712e1586339dfb34f6a26db
giautm <giau.tmg@gmail.com>
2020-06-30 14:30:46 +0700

    Bump version 1.0.3

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 module.exports = ({ url, iosManifest, config }) => {
   const { botToken, chatIds } = config;
   const fetch = require('node-fetch');
-
+  const qs = require('query-string')
   const releaseChannel = iosManifest.releaseChannel || 'default';
-  const queryString = releaseChannel === 'default' ? '' :
-    '?release-channel='+encodeURIComponent(releaseChannel);
-
-  const dataStr = encodeURIComponent(`${url}${queryString}`);
-  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${dataStr}`;
+  const publishedUrl = qs.stringifyUrl({
+    url,
+    query: releaseChannel === 'default' ? undefined : {
+      'release-channel': releaseChannel,
+    },
+  })
+  const qrUrl = qs.stringifyUrl({
+    url: 'https://api.qrserver.com/v1/create-qr-code',
+    query: {
+      data: publishedUrl,
+      size: '250x250',
+    }
+  })
 
   return Promise.all((chatIds || []).map((chatId) => {
     return fetch(`https://api.telegram.org/bot${botToken}/sendPhoto`, {
@@ -18,7 +26,7 @@ module.exports = ({ url, iosManifest, config }) => {
       body: JSON.stringify({
         chat_id: chatId,
         photo: qrUrl,
-        caption: `${iosManifest.name} v${iosManifest.version} published to ${dataStr}`,
+        caption: `${iosManifest.name} v${iosManifest.version} published to ${publishedUrl}`,
       }),
     }).then((response) => response.json()).then((data) => {
       if (data.ok) {
