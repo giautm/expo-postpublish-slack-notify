98552f5fb1f70fb5ff1773f4e5e0e1577fd336d1
Giau. Tran Minh <giau.tmg@gmail.com>
2020-10-12 13:43:06 +0700

    feat: support whitelist to send notifications

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-module.exports = ({ url, iosManifest, config }) => {
-  const { botToken, chatIds } = config;
-  const fetch = require('node-fetch');
-  const qs = require('query-string')
+const fetch = require('node-fetch')
+const qs = require('query-string')
+
+module.exports = ({ config, iosManifest, url }) => {
+  const { botToken, chatIds, whiteList = [] } = config || {};
+  const id = iosManifest.id
   const releaseChannel = iosManifest.releaseChannel || 'default';
+
+  if (Array.isArray(whiteList) && whiteList.length > 0) {
+    if (!whiteList.some(wl => wl.id == id && wl.releaseChannel === releaseChannel)) {
+      return 'Skip post notification';
+    }
+  }
+
   const publishedUrl = qs.stringifyUrl({
     url,
     query: releaseChannel === 'default' ? undefined : {
       'release-channel': releaseChannel,
     },
   })
-  const qrUrl = qs.stringifyUrl({
+  const caption = `${iosManifest.name} v${iosManifest.version} published to ${publishedUrl}`
+
+  const photo = qs.stringifyUrl({
     url: 'https://api.qrserver.com/v1/create-qr-code',
     query: {
       data: publishedUrl,
@@ -24,9 +35,9 @@ module.exports = ({ url, iosManifest, config }) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
+        caption,
         chat_id: chatId,
-        photo: qrUrl,
-        caption: `${iosManifest.name} v${iosManifest.version} published to ${publishedUrl}`,
+        photo,
       }),
     }).then((response) => response.json()).then((data) => {
       if (data.ok) {
